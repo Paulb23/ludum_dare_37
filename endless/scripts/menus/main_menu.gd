fbde94310b2a0b60f606bb0f8af6db9726c9940f
Paulb23 <p_batty@hotmail.co.uk>
2016-12-11 13:21:10 +0000

    Volume sliders

--- endless/scripts/menus/main_menu.gd
+++ endless/scripts/menus/main_menu.gd
@@ -3,6 +3,8 @@ extends Node2D
 func _ready():
 	get_node("play").connect("pressed", self, "play")
 	get_node("exit").connect("pressed", self, "exit")
+	get_node("music_vol").connect("value_changed", self, "music_vol")
+	get_node("sfx_vol").connect("value_changed", self, "sfx_vol")
 	pass
 
 func play():
@@ -17,4 +19,12 @@ func exit():
 	while get_node("SamplePlayer").is_voice_active(voice):
 		pass
 	get_tree().quit()
+	pass
+
+func music_vol(val):
+	AudioServer.set_stream_global_volume_scale(val)
+	pass
+
+func sfx_vol(val):
+	AudioServer.set_fx_global_volume_scale(val)
 	pass
